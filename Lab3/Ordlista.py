3bb0e1ebac8eae52b8e4ddfc46501e90a69c429a
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2025-10-02 14:19:11 +0200

    Inserted delete

--- Lab3/Ordlista.py
+++ Lab3/Ordlista.py
@@ -11,8 +11,9 @@ def printmeny():
   print("\nMenu for dictionary")
   print("1. Insert")
   print("2. Lookup")
-  print("3. Exit program")
-  return input("Choose alternative (1-3): ")
+  print("3. Delete")
+  print("4. Exit program")
+  return input("Choose alternative (1-4): ")
 
 # Simple dictionary program using lists of strings
 list1 = []
@@ -34,6 +35,16 @@ def lookup_mod1():
   if check_mod1(word):
     index = list1.index(word)
     print(f"Description for {word} : {list2[index]}")
+  else:
+    print(f"{word} not found in the dictionary.")
+
+def delete_mod1():
+  word = wordtolookup()
+  if check_mod1(word):
+    index = list1.index(word)
+    list1.pop(index)
+    list2.pop(index)
+    print(f"{word} has been deleted from the dictionary.")
   else:
     print(f"{word} not found in the dictionary.")
 
@@ -61,6 +72,14 @@ def lookup_mod2():
       return
   print(f"{word} not found in the dictionary.")
   
+def delete_mod2():
+  word = wordtolookup()
+  for i, (w, m) in enumerate(list_of_tuples):
+    if w == word:
+      list_of_tuples.pop(i)
+      print(f"{word} has been deleted from the dictionary.")
+      return
+  print(f"{word} not found in the dictionary.")
   
 # Dictionary program using dictionary data structure
 dictionary = {}
@@ -76,32 +95,48 @@ def insert_mod3():
   dictionary[itemword] = itemmeaning
 
 def lookup_mod3():
-  word = input("Enter the word to look up: ")
+  word = wordtolookup()
   if check_mod3(word):
     print(f"The meaning of {word} : {dictionary[word]}")
   else:
     print(f"{word} not found in the dictionary.")
+
+def delete_mod3():
+  word = wordtolookup()
+  if check_mod3(word):
+    del dictionary[word]
+    print(f"{word} has been deleted from the dictionary.")
+  else:
+    print(f"{word} not found in the dictionary.")
     
-def main_exec(ins,look):
+  """
+  Common execution function for all modules
+  insfunc = insert function
+  lookfunc = lookup function
+  delfunc = delete function
+  """
+def main_exec(insfunc, lookfunc, delfunc):
   meny = True
   while meny:
     val = printmeny()
 
     if val == '1':
-      ins()
+      insfunc()
     elif val == '2':
-      look()
+      lookfunc()
     elif val == '3':
+      delfunc()
+    elif val == '4':
       meny = False
       print("Exit program.")
     else:
       print("Invalid choice, please try again.")
     
 def main_mod1():
-  main_exec(insert_mod1, lookup_mod1)
+  main_exec(insert_mod1, lookup_mod1, delete_mod1)
       
 def main_mod2():
-  main_exec(insert_mod2, lookup_mod2)
+  main_exec(insert_mod2, lookup_mod2, delete_mod2)
       
 def main_mod3():
-  main_exec(insert_mod3, lookup_mod3)
+  main_exec(insert_mod3, lookup_mod3, delete_mod3)
